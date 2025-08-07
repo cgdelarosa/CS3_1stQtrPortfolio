1611467cc83f3d81035999300e43a0a8a500605c
cgdelarosa <b2029cgdelarosa@pshs.edu.ph>
2025-08-07 10:44:46 +0800

    1st non graded

---
+++ script.js
@@ -0,0 +1,6 @@
+let age = 17;
+let maxAge = 100;
+let numPerDay = 4;
+let totalRequired = (numPerDay * 365) * (maxAge - age)
+
+document.getElementByI("result").textcontext = "You will need " + totalRequired + "to last you until the ripe old age of " + maxAge + ".";
