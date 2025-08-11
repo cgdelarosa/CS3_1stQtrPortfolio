15d315a491289b88d37d2fb6cf5ffb5df8a8c264
cgdelarosa <b2029cgdelarosa@pshs.edu.ph>
2025-08-11 11:26:24 +0800

    Index.html change link code

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let maxAge = 100;
 let numPerDay = 4;
 let totalRequired = (numPerDay * 365) * (maxAge - age)
 
-document.getElementByI("result").textcontext = "You will need " + totalRequired + "to last you until the ripe old age of " + maxAge + ".";
+document.getElementByI("result").textcontext = "You will need " + totalRequired + "Chocolate Chip Cookies to last you until the ripe old age of " + maxAge + ".";
