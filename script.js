0b57bbb7f4dae16da1c6d768055e7456164d2e32
cgdelarosa <b2029cgdelarosa@pshs.edu.ph>
2025-08-11 11:28:24 +0800

    change script code

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let age = 17;
-let maxAge = 100;
-let numPerDay = 4;
-let totalRequired = (numPerDay * 365) * (maxAge - age)
 
-document.getElementByI("result").textcontext = "You will need " + totalRequired + "Chocolate Chip Cookies to last you until the ripe old age of " + maxAge + ".";
